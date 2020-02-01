1bf75d78d785aef12841aff456ab986ddd128fb6
demorales318 <demorales318@misena.edu.co>
2020-02-01 11:48:37 -0500

    mi primer commit

---
+++ iniciar_Secion.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>formulario</title>
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+        
+    </head>
+    <body>
+        <div id="contenedorform">
+            <h1 class="apren">iniciar secion</h1>
+            <div id="formulario">
+                <form method="post" action="logerUsuario.php">
+                    <label>usuario:</label>
+                    <input type="text" name="usuario" id="usuario" class="campo">
+                    <br>
+                    <div class"">
+                    <label>Contraseña</label>
+                    <input type="text" name="clave" id="clave" class="campo">
+                    <br>
+                    <br>
+                    <input type="submit" id="boton" value="enviar" >
+                        
+                    
+                </form>
+                
+            </div>
+        </div>
+    </body>
+</html>
